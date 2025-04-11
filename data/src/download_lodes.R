172858ed7ef559af0a80b01a278779163bcc27f8
Mitchell Henke <mitchell@mitchellhenke.com>
2025-04-11 11:31:59 -0500

    add script-running elements

--- data/src/download_lodes.R
+++ data/src/download_lodes.R
@@ -1,3 +1,13 @@
+# Rscript download_lodes.R YEAR STATE
+# Rscript download_lodes.R 2022 wi
+if(!require(readr)) {
+  install.packages("readr", repos = "http://cran.us.r-project.org")
+}
+
+args <- commandArgs(trailingOnly=T)
+
+year <- args[1]
+state <- args[2]
 
 # download the main and aux files, appending them
 download_lodes <- function(year, state, save = T){
@@ -7,10 +17,9 @@ download_lodes <- function(year, state, save = T){
                      state, "/od/", state, "_od_", "main", "_JT00_", year, ".csv.gz")
   aux.df <- readr::read_csv(aux.url, col_types = "ccnnnnnnnnnnc")
   main.df <- readr::read_csv(main.url, col_types = "ccnnnnnnnnnnc")
-  
   df <- rbind(aux.df, main.df)
   if(save == T){
-    
+
     dir.create(path = paste0("data/input/lodes/year=", year, "/state=", state),
                showWarnings = F, recursive = T)
     readr::write_csv(df, paste0("data/input/lodes/year=", year, "/state=", state,
@@ -19,4 +28,4 @@ download_lodes <- function(year, state, save = T){
   df
 }
 
-#download_lodes(2022, "wi")
+download_lodes(year, state)

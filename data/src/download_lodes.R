29138dd35272e2603278eda8fae8c1a8a0c185d7
jdjohn215 <jdjohn215@gmail.com>
2025-04-11 11:19:33 -0500

    init

---
+++ data/src/download_lodes.R
@@ -0,0 +1,22 @@
+
+# download the main and aux files, appending them
+download_lodes <- function(year, state, save = T){
+  aux.url <- paste0("https://lehd.ces.census.gov/data/lodes/LODES8/",
+                    state, "/od/", state, "_od_", "aux", "_JT00_", year, ".csv.gz")
+  main.url <- paste0("https://lehd.ces.census.gov/data/lodes/LODES8/",
+                     state, "/od/", state, "_od_", "main", "_JT00_", year, ".csv.gz")
+  aux.df <- readr::read_csv(aux.url, col_types = "ccnnnnnnnnnnc")
+  main.df <- readr::read_csv(main.url, col_types = "ccnnnnnnnnnnc")
+  
+  df <- bind_rows(aux.df, main.df)
+  if(save == T){
+    
+    dir.create(path = paste0("data/input/lodes/year=", year, "/state=", state),
+               showWarnings = F, recursive = T)
+    readr::write_csv(df, paste0("data/input/lodes/year=", year, "/state=", state,
+                                "/", state, ".csv.gz"))
+  }
+  df
+}
+
+#download_lodes(2022, "wi")

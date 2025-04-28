eaae25eb87f962eb12a38d46311d618b1113800b
jdjohn215 <jdjohn215@gmail.com>
2025-04-28 10:00:31 -0500

    simplify aggregation code

--- data/src/aggregate_lodes.R
+++ data/src/aggregate_lodes.R
@@ -39,36 +39,24 @@ aggregate_lodes <- function(year, state, geography, origin, save = T){
 
   # combine the main and auxillary files
   lodes <- bind_rows(lodes.main, lodes.aux)
+  
+  # fips codes can be shortened to specify larger geographies
+  #   this records the length of the FIPS code for the specified aggregation geo
+  fips.length <- case_when(
+    geography == "county" ~ 5,
+    geography == "tract" ~ 11,
+    geography == "block_group" ~ 12
+  )
 
-  if(geography == "tract"){
-    lodes.agg <- lodes |>
-      mutate(w_geo = str_sub(w_geocode, 1, 11),
-             h_geo = str_sub(h_geocode, 1, 11)) |>
-      group_by(w_geo, h_geo) |>
-      summarise(across(.cols = starts_with("S", ignore.case = F), .fns = sum),
-                .groups = "drop") |>
-      mutate(across(where(is.numeric), as.integer))
-  }
-
-  if(geography == "block_group"){
-    lodes.agg <- lodes |>
-      mutate(w_geo = str_sub(w_geocode, 1, 12),
-             h_geo = str_sub(h_geocode, 1, 12)) |>
-      group_by(w_geo, h_geo) |>
-      summarise(across(.cols = starts_with("S", ignore.case = F), .fns = sum),
-                .groups = "drop") |>
-      mutate(across(where(is.numeric), as.integer))
-  }
-
-  if(geography == "county"){
-    lodes.agg <- lodes |>
-      mutate(w_geo = str_sub(w_geocode, 1, 5),
-             h_geo = str_sub(h_geocode, 1, 5)) |>
-      group_by(w_geo, h_geo) |>
-      summarise(across(.cols = starts_with("S", ignore.case = F), .fns = sum),
-                .groups = "drop") |>
-      mutate(across(where(is.numeric), as.integer))
-  }
+  # aggregate blocks to the specified geography level by truncating the FIPS
+  #   codes and summarizing
+  lodes.agg <- lodes |>
+    mutate(w_geo = str_sub(w_geocode, 1, fips.length),
+           h_geo = str_sub(h_geocode, 1, fips.length)) |>
+    group_by(w_geo, h_geo) |>
+    summarise(across(.cols = starts_with("S", ignore.case = F), .fns = sum),
+              .groups = "drop") |>
+    mutate(across(where(is.numeric), as.integer))
 
   if(save == T){
     dir.create(path = paste0("intermediate/od_lodes/year=", year, "/geography=",
